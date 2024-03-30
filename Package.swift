8b2735424c232acba0d4a732c107e79183ab8cb7
jakkujakku <jakkujakku98@gmail.com>
2024-03-31 01:39:07 +0900

    init: 저장소 생성

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MoCoDotLibrary",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "MoCoDotLibrary",
+            targets: ["MoCoDotLibrary"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.0"),
+    ],
+    targets: [
+        .target(
+            name: "MoCoDotLibrary",
+            dependencies: ["SnapKit"]),
+    ],
+    swiftLanguageVersions: [.v5])
